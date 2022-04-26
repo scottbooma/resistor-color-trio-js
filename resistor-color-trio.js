0c4b969425b1ac262ee0eec20fb76e2df9fff072
scottbooma <scottbooma@gmail.com>
2022-04-26 11:30:24 -0400

    Pass 2 tests

--- resistor-color-trio.js
+++ resistor-color-trio.js
@@ -1,1 +1,18 @@
-export function decodedResistorValue() {}
+export function decodedResistorValue([color1, color2, color3]) {
+    const duoColors = `${colors[color1]}${colors[color2]}`
+    const zeros = `${10 ** colors[color3]}`
+    return `${duoColors * zeros} ohms`
+}
+
+const colors = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+}
